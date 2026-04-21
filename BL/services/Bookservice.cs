0d2248b20e6bb0f9d7c09b9194040edbff3c8c46
shereen-shaaban <sherinshaban67@gmail.com>
2026-04-21 07:19:51 +0200

    feat:add business tier(services) to the project and make a ref for Data Access TIer on it

---
+++ BL/services/Bookservice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.repositories;
+using librarysystem.Models;
+
+namespace BL.services
+{
+    public  class Bookservice
+    {
+       private IBase<Book> repo;
+        public Bookservice(IBase<Book> r)
+        {
+            repo = r;
+        }
+
+
+        public List<Book> Getall()
+        {
+            return repo.GetAll();
+        }
+
+        public void ADD(Book book)
+        {
+            repo.add(book);
+        }
+
+        public void Delete(int id)
+        {
+            var book = repo.search(id);
+            if (book != null)
+                repo.Delete(book);  
+        }
+
+        public Book Details(int id)
+        {
+            var book = repo.search(id);
+            if (book != null)
+                return book;
+            else
+                return null;
+		}
+
+  //      public void Edit(int id)
+  //      {
+		//	var book = repo.search(id);
+  //          if(book!=null)
+  //          {
+  //              repo.Delete(book);
+  //          }
+		//}
+
+
+
+    }
+}
